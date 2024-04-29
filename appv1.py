3dc271e26402cbc83a78b2f9e384e73dd5281e31
fyzan ahammad <fyzanahammad@gmail.com>
2024-04-29 16:07:55 +0530

    Update appv1.py

--- appv1.py
+++ appv1.py
@@ -1,12 +1,13 @@
-import streamlit as st
-from PyPDF2 import PdfReader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os
-from dotenv import load_dotenv
-from langchain_community.vectorstores import FAISS
-from langchain.vectorstores import FAISS
-from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
+import streamlit as st
 import google.generativeai as genai
+
+from PyPDF2 import PdfReader
+from dotenv import load_dotenv
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 
